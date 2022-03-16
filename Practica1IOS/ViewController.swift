dcc844784064763a2815a02c0cdf5154da75677e
oscarhluviano <oscarhluviano@gmail.com>
2022-03-15 23:04:07 -0600

    Initial Commit

---
+++ Practica1IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica1IOS
+//
+//  Created by OhLuvi on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
